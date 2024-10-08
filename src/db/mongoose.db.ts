aee71dc9dc753014b8f2495bb5db0d9f24bba5ac
charbelMansourDev <charbelmansour005@gmail.com>
2024-10-08 23:51:20 +0300

    +versioning

--- src/db/mongoose.db.ts
+++ src/db/mongoose.db.ts
@@ -6,7 +6,7 @@ export const connectMongoDB = async (app: any) => {
     app.listen(3000);
     set("strictQuery", false);
     await connect(variables.MONGO_URI);
-    console.log("MongoDB Connected!");
+    console.log(`MongoDB Connected! - Version: ${variables.VERSION}`);
     console.log("listening on: ", 3000);
   } catch (error) {
     console.log(error);
--- src/helpers/variables.ts
+++ src/helpers/variables.ts
@@ -1,7 +1,8 @@
 export const variables = {
-    MONGO_URI: process.env.MONGO_URI,
-    PORT: process.env.PORT || 3000,
-    SECRET: process.env.SECRET,
-    ADMIN: process.env.ADMIN,
-    USER: process.env.USER,
+  MONGO_URI: process.env.MONGO_URI,
+  PORT: process.env.PORT || 3000,
+  SECRET: process.env.SECRET,
+  ADMIN: process.env.ADMIN,
+  USER: process.env.USER,
+  VERSION: process.env.VERSION,
 };
